c2dc0aa9f382456d5427ad2a98d264094f899f87
Kaiwarefl4ke <109738567+Kaiwarefl4ke@users.noreply.github.com>
2022-10-09 18:55:38 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+register("worldLoad", () => {
+  let playerList = getAllPlayers();
+  if (playerListtoLowerCase().includes('necron')) {
+    break;
+  } else { 
+    new Thread(() => {
+      Thread.sleep(1);
+      ChatLib.chat(“message”);
+    }
+  }
+});
